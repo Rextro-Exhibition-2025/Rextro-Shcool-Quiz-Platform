f35e037bc545a71e338106d7f4bc8aeb40273785
Chamodpw2000 <143950123+Chamodpw2000@users.noreply.github.com>
2025-09-28 21:12:14 +0530

    Feature/add questions integration
    * feat: add questions integrated with authentication
    
    * refactor: unneeded variable declaration removed
    
    * fix: added an accedently removed function(seedquizzes)
    
    * feat: add questions integration completed
    
    * fix: build errors fixed
    
    * fix: change backend url
    
    * feat: attempt quiz integration completed

--- backend/controllers/quizController.ts
+++ backend/controllers/quizController.ts
@@ -1,5 +1,6 @@
 import Quiz from '../models/Quiz.js';
 import type { Request, Response } from 'express';
+import SchoolTeam from '../models/SchoolTeam.js';
 export const getQuizWithQuestions = async (req: Request, res: Response) => {
   try {
     const quizId = Number(req.params.quizId);
@@ -32,3 +33,94 @@ export const getQuizWithQuestions = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+function countCorrectAnswers(submittedAnswers: { questionId: number, answer: string }[], correctAnswers: { questionId: number, correctOption: string }[]): number {
+  let correctCount = 0;
+  submittedAnswers.forEach(sub => {
+    const correct = correctAnswers.find(ca => ca.questionId === sub.questionId);
+    if (correct && correct.correctOption.toLowerCase() === sub.answer.toLowerCase()) {
+      correctCount++;
+    }
+  });
+  return correctCount;
+}
+export const submitQuiz = async (req: Request, res: Response) => {
+  
+  try {
+    const { quizId, submittedAnswers  } = req.body;
+    const memberName = req.user?.memberName;
+    const schoolName = req.user?.schoolName;
+
+
+    
+
+    const quiz = await Quiz.findOne({ quizId }).populate({ path: 'questions', select: 'correctOption' });
+
+ 
+    const correctAnswers: { questionId: number, correctOption: string }[] = (quiz?.questions ?? []).map((q: any, idx: number) => ({
+      questionId: idx,
+      correctOption: q.correctOption.toLowerCase()
+  
+    }));
+
+
+
+    
+    if (!quiz) {
+      return res.status(404).json({ success: false, message: `Quiz with quizId ${quizId} not found.` });
+    }
+
+    const correctCount = countCorrectAnswers(submittedAnswers, correctAnswers);
+
+
+    const score = (correctCount/15)*100;
+
+    try {
+
+
+   await SchoolTeam.findOneAndUpdate(
+        { schoolName, "members.name": memberName },
+        { $set: { "members.$.marks": score } },
+        { new: true }
+      );
+
+
+      
+   
+      
+
+    } catch (error) {
+
+      console.error("Error submitting quiz:", error);
+      
+    }
+
+
+    
+
+
+
+    return res.status(200).json({
+      success: true,
+      message: 'Quiz submitted successfully',
+      data: {
+    
+        quizId,
+        score,
+   
+      }
+    });
+
+
+
+
+
+    
+
+  }catch (error) {
+
+
+
+    }
+}
