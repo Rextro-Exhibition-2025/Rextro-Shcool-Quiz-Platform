c014983eb6c1786e6112e98023e97e8fe8af6a95
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-10-07 18:10:33 +0530

    feat: Update API endpoints to use environment variable for server URL

--- frontend/interceptors/student.ts
+++ frontend/interceptors/student.ts
@@ -2,14 +2,14 @@
 import { useUser } from '@/contexts/UserContext';
 import axios from 'axios';
 
-export const createStudentApi = async ({token}:{token:string}) => {
+export const createStudentApi = async ({ token }: { token: string }) => {
     // Use client-side getSession instead of getServerSession
 
-  
- 
+
+
 
     const serverApi = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'http://localhost:3001/api',
+        baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'http://localhost:5000/api',
         headers: {
             'Content-Type': 'application/json',
         },
